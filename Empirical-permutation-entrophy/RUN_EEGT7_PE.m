3a9c604ed263af62479282fa05a2b61a7c2fd57e
ZJU-research <82191552+ZJU-research@users.noreply.github.com>
2021-05-27 14:37:54 +0800

    Delete Empirical-permutation-entrophy directory

--- Empirical-permutation-entrophy/RUN_EEGT7_PE.m
+++
@@ -1,6 +0,0 @@
-readEEG;
-ePE = PE(EEGEyeState.T7, 2, 6, 50);
-subplot(2,1,1)
-plot(EEGEyeState.T7);
-subplot(2,1,2)
-plot(ePE);
--- Empirical-permutation-entrophy/RUN_EEGdianxian_PE.m
+++
@@ -1,8 +0,0 @@
-[header, recorddata] = edfread('eeg10.edf');
-ePE = PE(recorddata(1, :), 2, 6, 50);
-% ePE = PEeq(recorddata(1, :), 2, 6, 50);
-% ePE = oldPE(recorddata(1, :), 2, 6, 50);
-subplot(2,1,1)
-plot(recorddata(1, :));
-subplot(2,1,2)
-plot(ePE);
