b8cc120b0acf1beb407917d2f4e63faff305aec7
ZJU-research <82191552+ZJU-research@users.noreply.github.com>
2021-05-27 15:18:51 +0800

    Update main.py

--- Permutation-Entropy/main.py
+++ Permutation-Entropy/main.py
@@ -1,15 +1,24 @@
+'''
+--------------------------------------- description--------------------------------------------------
+@author:Mengcheng Fang
+@time:2021.4.20
+@Use: Just run the funaction Permutation_Entropy(delay,m,data) directly.
+@Parameter Description:
+    delay:Time delay parameter, indicating the separation distance between data nodes.
+    m:The dimension of permutation entropy, the recommended value range is 3 to 7.
+    data:The input data, the requirement is that the data format is 1xn.
+'''
 import numpy as np
 import random
 from collections import Counter
 import math
 from matplotlib.pylab import plt
-#delay=2#时延为2
-#m=5#维度为，维度建议取值在3-7之间
-def Permutation_Entropy(delay,m,data):
+import pandas as pd
+def Permutation_Entropy(delay,m,data):#Permutation entropy function
     def key(a):
         return a[0]
     X=[[]for i in range(len(data)-(m-1)*delay)]
-    for i in range(len(X)):#将数据映射到m维空间
+    for i in range(len(X)):#Map data to m-dimensional space
         for j in range(m):
             X[i].append([data[i+j*delay],j])
         X[i].sort(key=key)
@@ -23,20 +32,20 @@ def Permutation_Entropy(delay,m,data):
     ordinal_patterns=Counter(ordinal_patterns)
     P=[]
     for key in ordinal_patterns.keys():
-        P.append(ordinal_patterns[key]/len(X))#计算序列模型概率分布
+        P.append(ordinal_patterns[key]/len(X))#Calculate the probability distribution of the sequence model
     H=0
     for i in range(len(P)):
         H+=P[i]*math.log(P[i])
     H*=-1.0
     return H/math.log(math.factorial(m))
 
-import pandas as pd
-df=pd.read_csv('data.csv')#加载数据
-delay=1#时延为1
-m=5#维度为5
-print('时延为:',delay,'  维度为:',m)
-for i in range(10):
-    data=df.iloc[:,i]
-    print('当前数据排列熵为：',Permutation_Entropy(1,5,data))
-    plt.plot(data)
-    plt.show()
+if __name__ == '__main__':
+    df=pd.read_csv('data.csv')#Download Data
+    delay=1#Delay is 1 
+    m=5#Dimension is set to 5
+    print('The delay is:',delay,'  The dimensions are:',m)
+    for i in range(10):
+        data=df.iloc[:,i]
+        print('The entropy of the current data arrangement is:',Permutation_Entropy(1,5,data))
+        plt.plot(data)
+        plt.show()
